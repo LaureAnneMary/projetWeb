545883a549e0de501e9d368a46efe958134a1904
lauraasld <laura.schlauder@viacesi.fr>
2018-04-16 14:53:38 +0200

    modif

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Utilisateurs</title>
+
+</head>
+
+<body>
+<ul>
+    <h1>Utilisateurs</h1>
+    @if(count($users)>0)
+        @foreach($users as $user)
+            <div class="well">
+                <h3><a href="/users/{{$user->id}}">{{$user->prenom}} {{$user->nom}}</a></h3>
+            </div>
+        @endforeach
+    @else
+        <p>Pas d'utilisateurs</p>
+    @endif
+
+</ul>
+</body>
+
+</html>
---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Utilisateurs</title>
+
+</head>
+
+<body>
+    <a href="/users">Retour</a>
+    <h1>{{$users->prenom}} {{$users->nom}}</h1>
+    <small>Créé le {{$users->created_at}}</small>
+<div>
+    {{--<@php(var_dump($users->intitule))>--}}
+    {{--<@php(die())>--}}
+
+    <h2>{{$users->rang_utilisateur->intitule}}</h2>
+</div>
+
+</body>
+
+</html>
